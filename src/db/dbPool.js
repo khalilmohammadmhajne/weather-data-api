e8b6fd00cc587c1fadf2cfca8ea33db0f6ecc494
khalilmohammadmhajne <khalil.mohammad.mhajne@gmail.com>
2024-11-24 01:34:25 +0200

    Update dbPool.js

--- src/db/dbPool.js
+++ src/db/dbPool.js
@@ -7,7 +7,7 @@ const config = {
   port: process.env.DB_PORT,
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME || 'weather_data_db',
 };
 
 const pool = mysql.createPool(config);
