4bfefbbecfa0cb89f1a1f82fd2b2c2f81d01b512
khalilmohammadmhajne <khalil.mohammad.mhajne@gmail.com>
2024-11-27 21:04:24 +0200

    edit ecosystem.config.js file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,60 +3,17 @@ module.exports = {
       {
         name: "start",
         script: "npm run start",
-        env: {
-          SERVER_PORT: 8080,
-          DB_HOST: "localhost",
-          DB_USER: "root",
-          DB_PASSWORD: "root",
-          DB_NAME: "weather_data_db",
-          DB_PORT: 3306,
-          DB_CONNECTION_LIMIT: 5,
-          REDIS_HOST: "localhost",
-          REDIS_PORT: 6379,
-          WEATHER_DATA_URL: "https://us-east1-climacell-platform-production.cloudfunctions.net/weather-data/batches",
-          MAX_RETRIES: 3,
-          RETRY_DELAY: 2000,
-          CONCURRENCY_LIMIT: 5
-        },
-        watch: true
+        env_file: ".env"
       },
       {
         name: "batchesCron",
         script: "npm run batchesCron",
-        env: {
-          SERVER_PORT: 8080,
-          DB_HOST: "localhost",
-          DB_USER: "root",
-          DB_PASSWORD: "root",
-          DB_NAME: "weather_data_db",
-          DB_PORT: 3306,
-          DB_CONNECTION_LIMIT: 5,
-          REDIS_HOST: "localhost",
-          REDIS_PORT: 6379,
-          WEATHER_DATA_URL: "https://us-east1-climacell-platform-production.cloudfunctions.net/weather-data/batches",
-          MAX_RETRIES: 3,
-          RETRY_DELAY: 2000,
-          CONCURRENCY_LIMIT: 5
-        }
+        env_file: ".env"
       },
       {
         name: "ingest",
         script: "npm run ingest",
-        env: {
-          SERVER_PORT: 8080,
-          DB_HOST: "localhost",
-          DB_USER: "root",
-          DB_PASSWORD: "root",
-          DB_NAME: "weather_data_db",
-          DB_PORT: 3306,
-          DB_CONNECTION_LIMIT: 5,
-          REDIS_HOST: "localhost",
-          REDIS_PORT: 6379,
-          WEATHER_DATA_URL: "https://us-east1-climacell-platform-production.cloudfunctions.net/weather-data/batches",
-          MAX_RETRIES: 3,
-          RETRY_DELAY: 2000,
-          CONCURRENCY_LIMIT: 5
-        }
+        env_file: ".env"
       }
     ]
   };
